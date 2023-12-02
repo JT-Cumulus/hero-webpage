718f3b226c2ff75a2093a98c7184dbf436eb6355
JT-Cumulus <jiatong1998@yahoo.com>
2023-12-02 17:50:52 +0100

    move analytics to root

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import { inject } from "@vercel/analytics";
-
-inject();
-
 const app = createApp(App);
 
 app.use(router);
